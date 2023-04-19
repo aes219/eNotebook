7ac0d06f1ea8006216c703f42fb5d1ecacdb706b
Vaidik Rungta <rungtavaidik@gmail.com>
2023-04-19 23:27:08 +0530

    Minor Changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,91 +1,90 @@
 import { useState } from 'react'
+require('dotenv').config
 //const history = createBrowserHistory();
 
 const Login = () => {
-  const { useNavigate } =require( 'react-router-dom')
-const navigate = useNavigate();
-    const [name, setName] = useState('')
-    const [pass, setPass] = useState('')
-    const [formError, setFormError] = useState({})
+  const { useNavigate } = require('react-router-dom')
+  const navigate = useNavigate();
+  const [name, setName] = useState('')
+  const [pass, setPass] = useState('')
+  const [formError, setFormError] = useState({})
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
+  const handleSubmit = (event) => {
+    event.preventDefault()
 
-        setFormError({})
+    setFormError({})
 
-        if (name !== 'admin') {
-            setFormError((prev) => ({
-                ...prev,
-                name: 'input input-bordered border-error',
-            }))
-        }
+    if (name !== process.env.USER) {
+      setFormError((prev) => ({
+        ...prev,
+        name: 'input input-bordered border-error',
+      }))
+    } else
 
-        if (pass !== 'vaidik@219') {
-            setFormError((prev) => ({
-                ...prev,
-                pass: 'input input-bordered border-error',
-            }))
-        } else {
-          navigate('/dash')
-        }
-    }
+      if (pass !== process.env.PASS) {
+        setFormError((prev) => ({
+          ...prev,
+          pass: 'input input-bordered border-error',
+        }))
+      } else {
+        navigate('/dash')
+      }
+  }
 
- 
 
-    return (
-        <div className='hero min-h-screen bg-base-200'>
-            <div className='hero-content flex-col lg:flex-row-reverse'>
-                <div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
-                    <div className='card-body'>
-                        <form onSubmit={handleSubmit}>
-                            <div className='form-control'>
-                                <label className='label'>
-                                    <span className='label-text'>Username</span>
-                                </label>
-                                <input
-                                    id='floatingInput'
-                                    type='text'
-                                    placeholder='Username'
-                                    className={`border  ${
-                                        formError['name']
-                                            ? formError['name']
-                                            : 'input input-bordered'
-                                    }`}
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                />
-                            </div>
-                            <div className='form-control'>
-                                <label className='label'>
-                                    <span className='label-text'>Password</span>
-                                </label>
-                                <input
-                                    id='floatingPassword'
-                                    type='password'
-                                    placeholder='Password'
-                                    value={pass}
-                                    className={`border ${
-                                        formError['pass']
-                                            ? formError['pass']
-                                            : 'input input-bordered'
-                                    }`}
-                                    onChange={(e) => setPass(e.target.value)}
-                                />
-                            </div>
-                            <div className='form-control mt-6'>
-                                <button
-                                    className='btn btn-outline no-animation btn-warning'
-                                    type='submit'
-                                >
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+          <div className='card-body'>
+            <form onSubmit={handleSubmit}>
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Username</span>
+                </label>
+                <input
+                  id='floatingInput'
+                  type='text'
+                  placeholder='Username'
+                  className={`border  ${formError['name']
+                      ? formError['name']
+                      : 'input input-bordered'
+                    }`}
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Password</span>
+                </label>
+                <input
+                  id='floatingPassword'
+                  type='password'
+                  placeholder='Password'
+                  value={pass}
+                  className={`border ${formError['pass']
+                      ? formError['pass']
+                      : 'input input-bordered'
+                    }`}
+                  onChange={(e) => setPass(e.target.value)}
+                />
+              </div>
+              <div className='form-control mt-6'>
+                <button
+                  className='btn btn-outline no-animation btn-warning'
+                  type='submit'
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Login
