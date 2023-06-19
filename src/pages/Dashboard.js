82fbf2d501f0515a250bc90e59a7f4bbc2eb88af
Vaidik Rungta <rungtavaidik@gmail.com>
2023-06-19 17:14:16 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,21 +1,13 @@
 import Navbar from "../components/Navbar"
-import Notes from "../components/Notes"
+import Note from "../components/Note"
 import CreateNote from "../components/CreateNote"
 import { useState } from "react"
 
 export const Dashboard = () => {
-    const [notes, setNotes] = useState([])
-    const addNote = (note) => {
-        setNotes([...notes, note])
-    }
-
     return(
         <div>
             <Navbar/>
-        <CreateNote onAddNote={addNote} notes={notes} setNotes={setNotes} />
-        {notes.map((note, index) => (
-          <Notes key={index} title={note.title} content={note.content} />
-        ))}
+          <Note />
       </div>
     )
 }
