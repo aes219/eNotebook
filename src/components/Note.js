7179ac11aeefb535b400e11199845552b174ee04
Vaidik Rungta <rungtavaidik@gmail.com>
2023-06-19 19:16:06 +0530

    Fix UI Bug

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,30 +1,21 @@
 import { useEffect, useState } from "react";
-
-const Card = ({ title, content }) => {
-  return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>{content}</p>
-      </div>
-    </div>
-  );
-};
+import Card from "./Card";
+import {Modal, Button} from 'react-daisyui'
 
 const Note = () => {
-  const [listing, setListing] = useState([
-    {
-      title: "Nothing here yet...",
-      content: "Start by creating a new note!"
-    }
-  ])
+  const [listing, setListing] = useState([])
+  const [visible, setVisible] = useState(false)
+  const [currentNote, setCurrentNote] = useState(null)
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
   useEffect(() => {
     const fetchData = async() => {
       const data = await fetch(`https://sheetdb.io/api/v1/5r7n7vrrrer7w`);
       const notes = await data.json();
       setListing(notes)
     }
-
     fetchData()
   }, [])
 
@@ -32,14 +23,34 @@ const Note = () => {
     <div className="join join-vertical lg:join-horizontal">
     
         {listing.map((a) => (
-          <div className="join-item">
-        <Card title={a.title} content={a.content}></Card>
+          <div className="join-item" style={{margin: '20px'}}>
+            <button onClick={() => {
+              setCurrentNote(a);
+              setVisible(true);
+            }}>
+        <Card title ={(a.title) ? a.title : 'Nothing here yet...'} content={(a.content) ? "" : 'Start by creating a new note...'}></Card>
+            </button>
+            <Modal open={visible}>
+        <Button
+          size="sm"
+          shape="circle"
+          className="absolute right-2 top-2"
+          onClick={toggleVisible}
+        >
+          ✕
+        </Button>
+        <Modal.Header className="font-bold">
+          {currentNote?.title}
+        </Modal.Header>
+
+        <Modal.Body>
+          {currentNote?.content}
+        </Modal.Body>
+      </Modal>
         </div>
       ))}
-      
     </div>
   );
 };
 
 export default Note
-
