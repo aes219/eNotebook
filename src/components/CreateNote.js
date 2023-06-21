ac50f35c775c645b8d3661288f362ff70653d7fc
Vaidik Rungta <rungtavaidik@gmail.com>
2023-06-21 22:17:52 +0530

    Migrate database from sheetDB to localstorage

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const CreateNote = () => {
   const [title, setTitle] = useState('');
@@ -7,108 +7,59 @@ const CreateNote = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (title.trim() && content.trim()) {
-      await fetch(`https://sheetdb.io/api/v1/5r7n7vrrrer7w`, {
-        method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-          body: JSON.stringify(
-            [
-              {
-                title: title,
-                content: content
-              }
-            ]
-          )
-      })
-      .then((res) => {
-      console.warn(res);
-        window.location = "/dash"
-    })
+      const notes = window.localStorage.getItem('notes');
+      const note = {
+        title,
+        content
+      };
+      const parsed = notes ? JSON.parse(notes) : [];
+      parsed.push(note);
+      console.log(`${parsed}`)
+      window.localStorage.setItem('notes', JSON.stringify(parsed));
+      window.location = "/dash";
     }
   }
+
   return (
-
-<div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-        <form onSubmit={handleSubmit}>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Title</span>
-          </label>
-          <input
-              type="text"
-              value={title}
-              placeholder="Type here"
-              className="input input-bordered"
-              id="notetitle"
-              onChange={(e) => setTitle(e.target.value)}
-            />
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input
+                  type="text"
+                  value={title}
+                  placeholder="Type here"
+                  className="input input-bordered"
+                  id="notetitle"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea
+                  placeholder="Type here"
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  className="textarea textarea-bordered textarea-lg w-full"
+                  id="notecontent"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-warning btn-outline no-animation">Create Note</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Content</span>
-          </label>
-          <textarea
-              placeholder="Type here"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              className="textarea textarea-bordered textarea-lg w-full"
-              id="notecontent"
-            />
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-warning btn-outline no-animation">Create Note</button>
-        </div>
-        </form>
       </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-
-/*
-    <div className="modal">
-      <div className="modal-box relative">
-        <label htmlFor="my-modal-3" className="btn btn-sm btn-ghost absolute right-2 top-2">✕</label>
-        <h3 className="text-lg font-bold">Create a New Note</h3>
-        <form onSubmit={handleSubmit}>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Title</span>
-            </label>
-            <input
-              type="text"
-              value={title}
-              placeholder="Type here"
-              className="input input-bordered"
-              id="notetitle"
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Content</span>
-            </label>
-            <textarea
-              placeholder="Type here"
-              value={content}
-              onChange={(e) => setTitle(e.target.value)}
-              className="textarea textarea-bordered textarea-lg w-full"
-              id="notecontent"
-            />
-          </div>
-          <div className="htmlForm-control mt-6">
-            <button className="btn btn-outline btn-warning no-animation" id="addnote" type="submit">Add Note</button>
-          </div>
-        </form>
-      </div>
-    </div>
-    */
-  )
-}
-
-export default CreateNote
+export default CreateNote;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,56 +1,29 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
-import {Modal, Button} from 'react-daisyui'
 
 const Note = () => {
-  const [listing, setListing] = useState([])
-  const [visible, setVisible] = useState(false)
-  const [currentNote, setCurrentNote] = useState(null)
-  const toggleVisible = () => {
-    setVisible(!visible)
-  }
+  const [listing, setListing] = useState([]);
 
   useEffect(() => {
-    const fetchData = async() => {
-      const data = await fetch(`https://sheetdb.io/api/v1/5r7n7vrrrer7w`);
-      const notes = await data.json();
-      setListing(notes)
+    const notes = window.localStorage.getItem('notes');
+    if (Array.isArray(JSON.parse(notes))) {
+      setListing(JSON.parse(notes));
+    } else {
+      setListing([]);
     }
-    fetchData()
-  }, [])
+  }, []);
 
   return (
-    <div className="join join-vertical lg:join-horizontal">
-    
-        {listing.map((a) => (
-          <div className="join-item" style={{margin: '20px'}}>
-            <button onClick={() => {
-              setCurrentNote(a);
-              setVisible(true);
-            }}>
-        <Card title ={(a.title) ? a.title : 'Nothing here yet...'} content={(a.content) ? "" : 'Start by creating a new note...'}></Card>
-            </button>
-            <Modal open={visible}>
-        <Button
-          size="sm"
-          shape="circle"
-          className="absolute right-2 top-2"
-          onClick={toggleVisible}
-        >
-          ✕
-        </Button>
-        <Modal.Header className="font-bold">
-          {currentNote?.title}
-        </Modal.Header>
-
-        <Modal.Body>
-          {currentNote?.content}
-        </Modal.Body>
-      </Modal>
+    <div className="items-center">
+      {listing.map((a) => (
+        <div style={{margin: '20px'}}>
+          <button>
+            <Card title={a.title} content={a.content}></Card>
+          </button>
         </div>
       ))}
     </div>
   );
 };
 
-export default Note
+export default Note;
