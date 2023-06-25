6feeed87e6c2a22dffdb9712429e9df2d43054ac
Vaidik Rungta <rungtavaidik@gmail.com>
2023-06-25 11:35:24 +0530

    + Add Modals to notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
+import Modal from './Modal';
 
 const Note = () => {
-  const [listing, setListing] = useState([]);
+  const [listing, setListing] = useState([])
+  const [currentNote, setCurrentNote] = useState(null)
+  const [cursor, setCursor] = useState("default")
+
+  const handleMouseOver = () => {
+    setCursor("pointer");
+  };
+
+  const handleMouseOut = () => {
+    setCursor("default");
+  };
+
 
   useEffect(() => {
     const notes = window.localStorage.getItem('notes');
@@ -15,29 +27,36 @@ const Note = () => {
   }, []);
 
   return (
-    <div className="items-center">
+    <div className="items-center flex flex-wrap justify-around h-min">
       {listing.map((a) => (
-        <div style={{ margin: '20px' }}>
+        <div>
           <button>
-            <Card
-            title={(a.title)?a.title:'Nothing here yet...'}
-            content={(a.content)?a.content:'Start by creating a new note, check above!'}
-            deleteNote={() => {
-              const newListing = listing.filter((note) => note !== a);
-              setListing(newListing);
-              window.localStorage.setItem('notes', JSON.stringify(newListing));
-            }}
-            editNote={() => {
-              window.localStorage.setItem("717l3", a.title);
-              window.localStorage.setItem("c0n73n7", a.content);
-              window.location = '/createnote'
-              const newListing = listing.filter((note) => note !== a);
-              setListing(newListing);
-              window.localStorage.setItem('notes', JSON.stringify(newListing));
-            }}
+            <label
+              for="my_modal_6" onClick={() => { setCurrentNote(a) }}
+              style={{ cursor: cursor }}
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
             >
-            </Card>
+              <Card
+                title={a.title}
+                deleteNote={() => {
+                  const newListing = listing.filter((note) => note !== a);
+                  setListing(newListing);
+                  window.localStorage.setItem('notes', JSON.stringify(newListing));
+                }}
+                editNote={() => {
+                  window.localStorage.setItem("717l3", a.title);
+                  window.localStorage.setItem("c0n73n7", a.content);
+                  window.location = '/createnote'
+                  const newListing = listing.filter((note) => note !== a);
+                  setListing(newListing);
+                  window.localStorage.setItem('notes', JSON.stringify(newListing));
+                }}
+              >
+              </Card>
+            </label>
           </button>
+          <Modal title={currentNote?.title} content={currentNote?.content} />
         </div>
       ))}
     </div>
