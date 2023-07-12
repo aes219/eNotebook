249e8b0d530ae8301f99989796f068bd722a9cc3
Vaidik Rungta <rungtavaidik@gmail.com>
2023-07-12 16:08:22 +0530

    Add HTML support for notes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,7 @@ const Modal = ({ title, content }) => {
                 <div className="modal-box">
                     <label htmlFor="my_modal_6" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</label>
                     <h3 className="font-bold text-lg">{title}</h3>
-                    <p className="py-4">{content}</p>
+                    <p className="py-4" dangerouslySetInnerHTML={{__html: content}}/>
                 </div>
             </div>
         </div>
