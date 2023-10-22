03dfdb3cdc75965e87c4f581ac4b70ab4d70d2a5
Vaidik <rungtavaidik@gmail.com>
2023-10-22 10:54:30 +0530

    Update Note.js
    Add welcome message for empty grid.

--- src/components/Note.js
+++ src/components/Note.js
@@ -9,6 +9,12 @@ const Note = () => {
   const [currentNote, setCurrentNote] = useState(null)
   const [cursor, setCursor] = useState("default")
 
+  const navigate = () => {
+    window.localStorage.setItem("717l3", "")
+    window.localStorage.setItem("c0n73n7", "")
+    direct('/createNote')
+  }
+
   const handleMouseOver = () => {
     setCursor("pointer");
   };
@@ -16,7 +22,6 @@ const Note = () => {
   const handleMouseOut = () => {
     setCursor("default");
   };
-
 
   useEffect(() => {
     const notes = window.localStorage.getItem('notes');
@@ -27,42 +32,65 @@ const Note = () => {
       setListing([]);
     }
   }, []);
-
-  return (
-    <div className="ml-8 grid gap-4 grid-cols-3">
-      {listing.map((a) => (
-        <div>
-          <button>
-            <label
-              htmlFor="my_modal_6" onClick={() => { setCurrentNote(a) }}
-              style={{ cursor: cursor }}
-              onMouseOver={handleMouseOver}
-              onMouseOut={handleMouseOut}
-            >
-              <Card
-                title={a.title}
-                deleteNote={() => {
-                  const newListing = listing.filter((note) => note !== a);
-                  setListing(newListing);
-                  window.localStorage.setItem('notes', JSON.stringify(newListing));
-                }}
-                editNote={() => {
-                  window.localStorage.setItem("717l3", a.title);
-                  window.localStorage.setItem("c0n73n7", a.content);
-                  direct('/createnote')
-                  const newListing = listing.filter((note) => note !== a);
-                  setListing(newListing);
-                  window.localStorage.setItem('notes', JSON.stringify(newListing));
-                }}
-              >
-              </Card>
-            </label>
-          </button>
-          <Modal title={currentNote?.title} content={currentNote?.content} />
-        </div>
-      ))}
+  
+  if(window.localStorage.getItem('notes') === "[]") {
+    return (
+      <>
+      <div class="hero bg-base-200 h-screen -mt-16">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Nothing here yet...</h1>
+      <p class="py-6">Get started by clicking below!</p>
+      <label className="btn gap-2 btn-warning" htmlFor="my-modal-3" onClick={navigate}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#3d2800" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+          New Note
+        </label>
     </div>
-  );
-};
+  </div>
+</div>
+      </>
+    )
+  } else {
+  return (
+    <>
+      <div className="ml-8 grid grid-cols-3 gap-4">
+        {listing.map((a) => (
+          <div>
+            <button>
+              <label
+                htmlFor="my_modal_6"
+                onClick={() => { setCurrentNote(a) }}
+                style={{ cursor: cursor }}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+              >
+                <Card
+                  title={a.title}
+                  deleteNote={() => {
+                    const newListing = listing.filter((note) => note !== a);
+                    setListing(newListing);
+                    window.localStorage.setItem('notes', JSON.stringify(newListing))
+                    window.location.reload()
+                  }}
+                  editNote={() => {
+                    window.localStorage.setItem("717l3", a.title);
+                    window.localStorage.setItem("c0n73n7", a.content);
+                    direct('/createnote')
+                    const newListing = listing.filter((note) => note !== a);
+                    setListing(newListing);
+                    window.localStorage.setItem('notes', JSON.stringify(newListing));
+                  }}
+                >
+                </Card>
+              </label>
+            </button>
+            <Modal title={currentNote?.title} content={currentNote?.content} />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+}
 
-export default Note;
+export default Note
